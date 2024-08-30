adbbc450f0859208579869b362f572d6a0e3e04a
Rohan Rama Poojari <123969188+Cetyl@users.noreply.github.com>
2024-08-30 15:37:08 +0200

    Create aws-deploy.sh

---
+++ aws-deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Variables
+CLUSTER_NAME="my-cluster"
+SERVICE_NAME="my-service"
+TASK_DEFINITION="my-task-def"
+REGION="us-west-2"
+
+# Register task definition
+aws ecs register-task-definition \
+  --family $TASK_DEFINITION \
+  --container-definitions '[{"name":"my-container","image":"my-dockerhub-username/my-web-app:latest","memory":512,"cpu":256,"essential":true}]' \
+  --region $REGION
+
+# Update service
+aws ecs update-service \
+  --cluster $CLUSTER_NAME \
+  --service $SERVICE_NAME \
+  --task-definition $TASK_DEFINITION \
+  --region $REGION
