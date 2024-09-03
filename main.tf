5c834853908372c40bd9724c07ae8cedad7b37ea
Rohan Rama Poojari <123969188+Cetyl@users.noreply.github.com>
2024-09-03 13:19:02 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -162,4 +162,39 @@ resource "aws_lb_target_group" "my_web_app_tg" {
   }
 }
 
+# Define the IAM role for ECS task execution
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs_execution_role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# Define the IAM role for ECS tasks
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs_task_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
